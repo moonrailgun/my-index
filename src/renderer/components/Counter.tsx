5c999ef7359b1678fe7e02333fd6d3f5304ed688
moonrailgun <moonrailgun@gmail.com>
2019-08-21 10:09:41 +0800

    change indent to 2

--- src/renderer/components/Counter.tsx
+++ src/renderer/components/Counter.tsx
@@ -4,27 +4,27 @@ require('./Counter.scss');
 const redCubeImg = require('./RedCube.jpg');
 
 export interface Props {
-    value: number;
+  value: number;
 
-    incrementValue: () => any;
-    decrementValue: () => any;
+  incrementValue: () => any;
+  decrementValue: () => any;
 }
 
 const Counter: React.FunctionComponent<Props> = ({ value, incrementValue, decrementValue }) => (
-    <div className="counter">
-        <p>
-            <img src={redCubeImg} />
-        </p>
-        <p id="counter-value">Current value: {value}</p>
-        <p>
-            <button id="increment" onClick={incrementValue}>
-                Increment
-            </button>
-            <button id="decrement" onClick={decrementValue}>
-                Decrement
-            </button>
-        </p>
-    </div>
+  <div className="counter">
+    <p>
+      <img src={redCubeImg} />
+    </p>
+    <p id="counter-value">Current value: {value}</p>
+    <p>
+      <button id="increment" onClick={incrementValue}>
+        Increment
+      </button>
+      <button id="decrement" onClick={decrementValue}>
+        Decrement
+      </button>
+    </p>
+  </div>
 );
 
 export default Counter;
--- test/components/Counter.spec.tsx
+++ test/components/Counter.spec.tsx
@@ -4,10 +4,10 @@ import * as renderer from 'react-test-renderer';
 import Counter from '../../src/renderer/components/Counter';
 
 describe('Counter component', () => {
-    it('renders correctly', () => {
-        const tree = renderer
-            .create(<Counter value={1} incrementValue={jest.fn()} decrementValue={jest.fn()} />)
-            .toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Counter value={1} incrementValue={jest.fn()} decrementValue={jest.fn()} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
