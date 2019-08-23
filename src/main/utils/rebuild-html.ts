be25bd704c0b17e96bc212eeb55080fc959b0972
moonrailgun <moonrailgun@gmail.com>
2019-08-23 14:55:42 +0800

    rebuild html

--- src/main/utils/rebuild-html.ts
+++ src/main/utils/rebuild-html.ts
@@ -1,4 +1,14 @@
-import { js2xml, xml2js, ElementCompact, Options } from 'xml-js';
+import { js2xml, xml2js, Element, Options } from 'xml-js';
+import _get from 'lodash/get';
+import _set from 'lodash/set';
+import _cloneDeep from 'lodash/cloneDeep';
+import urlUtils from 'url';
+import { transformScriptUrl } from './url-transform';
+
+interface PageContext {
+  url: string;
+  assets?: string[];
+}
 
 const commonOptions: Options.XML2JS & Options.JS2XML = {
   compact: false,
@@ -8,18 +18,73 @@ const commonOptions: Options.XML2JS & Options.JS2XML = {
  * html转js对象
  * @param html html文本
  */
-export const html2js = (html: string) => {
+export const html2js = (html: string): Element => {
   const obj = xml2js(html, { ...commonOptions, lowercase: true, strict: false });
 
-  return obj;
+  return obj as Element;
 };
 
 /**
  * js对象转html文本
  * @param obj 由html2js生成的文本
  */
-export const js2html = (obj: Element | ElementCompact) => {
+export const js2html = (obj: Element): string => {
   const html = js2xml(obj, commonOptions);
 
   return html;
+};
+
+export const rebuildHtml = (htmlStr: string, pageContext: Readonly<PageContext>) => {
+  const obj = html2js(htmlStr);
+  const context = _cloneDeep(pageContext);
+  travelNode(obj, context);
+
+  return {
+    html: js2html(obj),
+    assets: context.assets,
+  };
+};
+
+/**
+ * 遍历ast， 并对其中特定类型的节点进行处理
+ * @param node 节点元素
+ */
+export const travelNode = (node: Element, context: PageContext) => {
+  if (!context.assets) {
+    context.assets = [];
+  }
+
+  const type = node.type;
+  const name = node.name;
+  if (type === 'element' && name === 'script') {
+    // 处理script标签
+    let scriptSrc: string = _get(node, 'attributes.src');
+    if (!scriptSrc) {
+      // 不处理没有src的标签
+      return;
+    }
+
+    if (scriptSrc.startsWith('//')) {
+      scriptSrc = `http:${scriptSrc}`;
+    }
+
+    // 将地址标准化为绝对路径
+    const { host, pathname } = urlUtils.parse(scriptSrc); // 使用pathname不包括querystring
+    if (host === null) {
+      // 是一个内部地址
+      if (pathname && pathname.startsWith('/')) {
+        // path是一个绝对路径
+        scriptSrc = urlUtils.resolve(context.url, pathname);
+      }
+    }
+
+    const scriptLocalPath = transformScriptUrl(scriptSrc);
+    // TODO: 需要处理相对当前网页的路径，或者file协议
+
+    _set(node, 'attributes.src', scriptLocalPath);
+    context.assets.push(scriptLocalPath);
+  }
+
+  const children = node.elements || [];
+  children.forEach(child => travelNode(child, context));
 };
--- test/units/rebuild-html.spec.ts
+++ test/units/rebuild-html.spec.ts
@@ -1,4 +1,4 @@
-import { html2js } from '../../src/main/utils/rebuild-html';
+import { html2js, rebuildHtml } from '../../src/main/utils/rebuild-html';
 
 // baidu html
 const testHtml = `<html>
@@ -6,7 +6,7 @@ const testHtml = `<html>
 
 	<script>
 		location.replace(location.href.replace("https://","http://"));
-	</script>
+  </script>
 </head>
 <body>
 	<noscript><meta http-equiv="refresh" content="0;url=http://www.baidu.com/"></noscript>
@@ -23,5 +23,23 @@ describe('rebuild-html', () => {
     expect(typeof obj).toBe('object');
     expect(obj).toHaveProperty('elements');
     expect(JSON.stringify(obj)).toBe(targetStr);
+  });
+
+  it('should be parse non-strict xml', async () => {
+    const html = '<html><head><meta http-equiv="Cache-Control" content="no-siteapp"></head></html>';
+    const json =
+      // tslint:disable-next-line: max-line-length
+      '{"elements":[{"type":"element","name":"html","elements":[{"type":"element","name":"head","elements":[{"type":"element","name":"meta","attributes":{"http-equiv":"Cache-Control","content":"no-siteapp"}}]}]}]}';
+
+    expect(JSON.stringify(html2js(html))).toBe(json);
+  });
+
+  it('should be rebuild js script', async () => {
+    const sourceHtml = '<html><head><script src="/test.js"></script></head></html>';
+    const targetHtml = '<html><head><script src="domain.com/test.js"/></head></html>';
+
+    const dest = rebuildHtml(sourceHtml, { url: 'http://domain.com' });
+    expect(dest.html).toBe(targetHtml);
+    expect(dest.assets).toEqual(['domain.com/test.js']);
   });
 });
